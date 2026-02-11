57234f2db629a6245a657a0a86a64b05eb240b0e
Krishan Gupta <68317094+krishan-gupta@users.noreply.github.com>
2026-02-11 22:57:44 +0530

    Time: 0 ms (100%), Space: 8 MB (48.88%) - LeetHub

--- 2469-convert-the-temperature/2469-convert-the-temperature.c
+++ 2469-convert-the-temperature/2469-convert-the-temperature.c
@@ -3,10 +3,10 @@
  */
 double* convertTemperature(double celsius, int* returnSize) {
     double *arr = (double*)malloc(2*sizeof(double));
-    double kelvin = celsius + 273.15;
-    double fahren = celsius*1.80 + 32.00;
-    arr[0]=kelvin;
-    arr[1]=fahren;
+    arr[0] = celsius + 273.15;
+    arr[1] = celsius*1.80 + 32.00;
+
     *returnSize = 2;
     return arr;
+    free(arr);
 }
